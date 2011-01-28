b36ec55dd4733eaec969e6acb564a98c86e979f1
BillRowcliffe <ieguitarist@gmail.com>
2011-01-27 23:37:45 -0600

    Bill Signed-off-by: BillRowcliffe <ieguitarist@gmail.com>

--- Project1/Bill_Sandbox.r
+++ Project1/Bill_Sandbox.r
@@ -1,81 +1,86 @@
 #Load  LM
-BB<-read.csv("bb-2009-all.csv")
+bb<-read.csv("bb-2009-all.csv")
 
 #Generate_Statistics
-fgprct<-BB$fgm/BB$fga
-ftprct<-BB$ftm/BB$fga
-tpprct<-BB$tpm/BB$tpa
-minpergame<-BB$minutes/BB$gp
+fgprct<-bb$fgm/bb$fga
+ftprct<-bb$ftm/bb$fga
+tpprct<-bb$tpm/bb$tpa
+minpergame<-bb$minutes/bb$gp
+summary(lm(salary~minpergame,data=bb))
+tpmpergame<-bb$tpm/bb$gp
+summary(lm(salary~tpmpergame,data=bb))
+fgmpergame<-bb$fgm/bb$gp
+summary(lm(salary~fgmpergame,data=bb))
 
 #Summary
-Summary(BB)
-summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct+fgprct+ftprct+minpergame,data=BB))
+Summary(bb)
+summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct+fgprct+ftprct+minpergame+tpmpergame+fgmpergame,data=bb))
 
 #Partition Minutes
 library(ggplot2)
-qplot(BB$minutes,data=BB,geom="histogram",xlab="Minutes",breaks=seq(0,3500,125))
-qplot(BB$salary,data=BB,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
-summary(lm(salary~minutes,data=BB))
+qplot(bb$minutes,data=bb,geom="histogram",xlab="Minutes",breaks=seq(0,3500,125))
+qplot(bb$salary,data=bb,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
+summary(lm(salary~minutes,data=bb))
 
 #Scatter note: non-functioning line
 tmp<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*x))
 tmp2<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*1))
-qplot(jitter(minutes),salary,data=BB,geom="point",xlab="Min",ylab="Salary")+
+qplot(jitter(minutes),salary,data=bb,geom="point",xlab="Min",ylab="Salary")+
   layer(data=tmp2,geom="line",mapping=aes(x=x,y=y),colour="grey60",size=1)+
   layer(data=tmp,geom="line",mapping=aes(x=x,y=y),colour="grey20",size=1)
 
 #Remove instances with less than 750 minutes.
-BB_MinGT750<-subset(BB, (minutes>=750))
+bb_MinGT750<-subset(bb, (minutes>=750))
 
-summary(lm(salary~minutes,data=BB_MinGT750))
-qplot(BB_MinGT750$minutes,data=BB_MinGT750,geom="histogram",xlab="Minutes",breaks=seq(0,3500,125))
-qplot(BB_MinGT750$salary,data=BB_MinGT750,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
+summary(lm(salary~minutes,data=bb_MinGT750))
+qplot(bb_MinGT750$minutes,data=bb_MinGT750,geom="histogram",xlab="Minutes",breaks=seq(0,3500,125))
+qplot(bb_MinGT750$salary,data=bb_MinGT750,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
 
 #Scatter
 tmp<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*x))
 tmp2<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*1))
-qplot(jitter(minutes),salary,data=BB_MinGT750,geom="point",xlab="Min",ylab="Salary")+
+qplot(jitter(minutes),salary,data=bb_MinGT750,geom="point",xlab="Min",ylab="Salary")+
   layer(data=tmp2,geom="line",mapping=aes(x=x,y=y),colour="grey60",size=1)+
   layer(data=tmp,geom="line",mapping=aes(x=x,y=y),colour="grey20",size=1)
 
 #General Summary greater than 750 minutes
-fgprct2<-BB_MinGT750$fgm/BB_MinGT750$fga
-ftprct2<-BB_MinGT750$ftm/BB_MinGT750$fga
-tpprct2<-BB_MinGT750$tpm/BB_MinGT750$tpa
-summary(BB_MinGT750)
-summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct2+fgprct2+ftprct2,data=BB_MinGT750))
+fgprct2<-bb_MinGT750$fgm/bb_MinGT750$fga
+ftprct2<-bb_MinGT750$ftm/bb_MinGT750$fga
+tpprct2<-bb_MinGT750$tpm/bb_MinGT750$tpa
+summary(bb_MinGT750)
+summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct2+fgprct2+ftprct2,data=bb_MinGT750))
 
 #General Summary less than 750 minutes
-BB_MinLT750<-subset (BB, (minutes<750))
-fgprct3<-BB_MinLT750$fgm/BB_MinLT750$fga
-ftprct3<-BB_MinLT750$ftm/BB_MinLT750$fga
-tpprct3<-BB_MinLT750$tpm/BB_MinLT750$tpa
-summary(BB_MinLT750)
-summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct3+fgprct3+ftprct3,data=BB_MinLT750))
+bb_MinLT750<-subset (bb, (minutes<750))
+fgprct3<-bb_MinLT750$fgm/bb_MinLT750$fga
+ftprct3<-bb_MinLT750$ftm/bb_MinLT750$fga
+tpprct3<-bb_MinLT750$tpm/bb_MinLT750$tpa
+summary(bb_MinLT750)
+summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct3+fgprct3+ftprct3,data=bb_MinLT750))
 
 #Salary VS Position
 library(ggplot2)
-qplot(BB$position,data=BB,geom="histogram",xlab="position")
-qplot(BB$salary,data=BB,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
-summary(lm(salary~position,data=BB))
+qplot(bb$position,data=bb,geom="histogram",xlab="position")
+qplot(bb$salary,data=bb,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
+summary(lm(salary~position,data=bb))
 
 #Scatter note: non-functioning line
 tmp<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*x))
 tmp2<-data.frame(x=c(0,3500,250),y=c(250000,250000+2000*1))
-qplot(jitter(position),salary,data=BB,geom="point",xlab="Min",ylab="Salary")+
+qplot(jitter(position),salary,data=bb,geom="point",xlab="Min",ylab="Salary")+
   layer(data=tmp2,geom="line",mapping=aes(x=x,y=y),colour="grey60",size=1)+
   layer(data=tmp,geom="line",mapping=aes(x=x,y=y),colour="grey20",size=1)
 
 #double instances with position="center".
-BB_2Center<-read.csv(file.choose())
-summary(lm(salary~position,data=BB_2Center))
-qplot(BB_2Center$position,data=BB_2Center,geom="histogram",xlab="position",breaks=seq(0,3500,125))
-qplot(BB_2Center$salary,data=BB_2Center,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
+bb_2Center<-read.csv(file.choose())
+summary(lm(salary~position,data=bb_2Center))
+qplot(bb_2Center$position,data=bb_2Center,geom="histogram",xlab="position",breaks=seq(0,3500,125))
+qplot(bb_2Center$salary,data=bb_2Center,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
 
 #Scatter
 tmp<-data.frame(x=c(250000),y=c("Center","Forward","Guard"))
 tmp2<-data.frame(x=c(250000),y=c("Center","Forward","Guard"))
-qplot(salary,position,data=BB_2Center,geom="point",xlab="Salary",ylab="position")+
+qplot(salary,position,data=bb_2Center,geom="point",xlab="Salary",ylab="position")+
   layer(data=tmp2,geom="line",mapping=aes(x=x,y=y),colour="grey60",size=1)+
   layer(data=tmp,geom="line",mapping=aes(x=x,y=y),colour="grey20",size=1)
 
