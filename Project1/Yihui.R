a832ff9165c12c67e743172aab74674d25d1576d
Yihui Xie <xie@yihui.name>
2011-01-28 01:42:11 -0600

    preliminary regression results

--- Project1/Yihui.R
+++ Project1/Yihui.R
@@ -1,3 +1,11 @@
-## my code here
-1+1
+fit = lm(salary ~ . - reb - tpm, data = subset(bb, select = team:salary))
+fit.coef = coef(summary(fit))
+fit.coef[fit.coef[, 4] < .1, ]
 
+round(coef(summary(fit1<-step(fit, trace=0))), 2)
+par(mfrow=c(1,2))
+plot(fit1, which=1:2)
+
+summary(fit2<-step(update(fit, log(salary)~.), trace=0))
+par(mfrow=c(1,2))
+plot(fit2, which=1:2)
