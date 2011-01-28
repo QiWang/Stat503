26a1f39435c243bd9651dc74bcfa93f19a511f41
Yihui Xie <xie@yihui.name>
2011-01-27 22:13:17 -0600

    changed bb.new to bb for consistency with others

--- Project1/preprocess.R
+++ Project1/preprocess.R
@@ -7,6 +7,6 @@ dup.players = with(bb, ilkid %in% ilkid[duplicated(ilkid)])
 bb[dup.players, c(1:3, 5, 25, 26)]
 print(qplot(minutes, salary, data = bb, color = factor(dup.players), size=as.integer(dup.players)) + scale_colour_hue('duplicate')+ scale_size(to=c(1,2),legend = FALSE))
 
-bb.new = bb[!dup.players, -c(4, 6, 7)]
+bb = bb[!dup.players, -c(4, 6, 7)]
 
-## now work on bb.new
+## now work on bb
