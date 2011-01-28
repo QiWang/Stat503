216a544d5bc227c4c44c9cb34f5b5fc0866302ad
BillRowcliffe <ieguitarist@gmail.com>
2011-01-28 00:54:35 -0600

    Added "per game" stats
    Expanded regression analysis
    
    Signed-off-by: BillRowcliffe <ieguitarist@gmail.com>

--- Project1/Qi.R
+++ Project1/Qi.R
@@ -5,14 +5,21 @@ bb<-read.csv('bb-2009-all-correct.csv')
 fgprct<-bb$fgm/bb$fga
 ftprct<-bb$ftm/bb$fga
 tpprct<-bb$tpm/bb$tpa
+minpergame<-bb$minutes/bb$gp
+
+tpmpergame<-bb$tpm/bb$gp
+
+fgmpergame<-bb$fgm/bb$gp
+
 
 #Summary
 summary(bb)
 
 
 library(ggplot2)
-## get started with tons of plots and tables...
+## Initial Exploration Plots
 qplot(bb$salary,data=bb,geom="histogram",xlab="salary",breaks=seq(0,23000000,500000))
+#As expected, the salary histogram is severely skewed to the right. The range of salaries is very large, and often there are only a few key players to a team
 qplot(minutes, salary, data = bb, geom = c('point', 'smooth'))
 qplot(asts, salary, data = bb, geom = c('point', 'smooth'))
 qplot(pf, salary, data = bb, geom = c('point', 'smooth'))
@@ -33,9 +40,13 @@ qplot(reb, salary, data = bb, geom = c('point', 'smooth'), facets=~team)
 
 
 
-# Regreession
-bb_allfit <- lm(salary~gp+minutes+pts+oreb+dreb+reb+asts+stl+blk+turnover+pf+fga+fgm+fta+ftm+tpa+tpm,data=bb)
-summary(bb_allfit)
+# Regression
+
+summary(lm(salary~minutes+position+fgm+fga+ftm+fta+tpm+tpm+tpprct+fgprct+ftprct+minpergame+tpmpergame+fgmpergame,data=bb))
+
+summary(lm(salary~gp + minutes + position + pts + oreb + dreb + reb + 
+    asts + stl + blk + turnover + pf + fga + fgm + fta + ftm + 
+    tpa + tpm + minpergame + tpmpergame + fgmpergame, data = bb))
 
 bb_rebfit <- lm(salary~minutes,data=bb)
 summary(bb_rebfit)
@@ -43,7 +54,21 @@ summary(bb_rebfit)
 bb_tpmfit <- lm(salary~asts,data=bb)
 summary(bb_tpmfit)
 
-bb_6fit <- lm(salary~minutes+gp+oreb+dreb+asts+stl+turnover,data=bb)
+summary(lm(salary~position,data=bb))
+
+summary(lm(salary~dreb,data=bb))
+
+summary(lm(salary~dreb,data=bb))
+
+summary(lm(salary~fta,data=bb))
+
+summary(lm(salary~minpergame,data=bb))
+
+summary(lm(salary~fgmpergame,data=bb))
+
+summary(lm(salary~tpmpergame,data=bb))
+
+bb_6fit <- lm(salary~minpergame+gp+oreb+dreb+asts+stl+turnover,data=bb)
 summary(bb_6fit)
 
 # model searching
